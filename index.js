9837d7d995fa61b67ade91e46527109868666189
Topinkaverdan <veronikadanilova439@gmail.com>
2023-05-11 04:04:00 +0300

    Выносит форму входа в отдельный компонент

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { fetchDelete, fetchGet, fetchPost } from "./api.js";
+import { renderLoginComponent } from "./components/login-component.js";
  let tasks = [];
 
     let token = "Bearer bgc0b8awbwas6g5g5k5o5s5w606g37w3cc3bo3b83k39s3co3c83c03ck";
@@ -21,37 +22,12 @@ import { fetchDelete, fetchGet, fetchPost } from "./api.js";
       
       if (!token) {
 
-        const appHtml = `<h3 class="form-title">Форма регистрации</h3>
-                <div class="form-row">
-                    Логин
-                <input
-                type="text"
-                id="login-input"
-                class="input"
-                />
-                <br/><br/>
-                Пароль 
-                <input
-                type="text"
-                id="login-input"
-                class="input"
-                />
-                </div>
-                <br />
-                <button class="button" id="login-button">Войти</button>
-                    </div>`
-        
-        appEl.innerHTML = appHtml;
-        document.getElementById("login-button").addEventListener('click', () => {
-
-            token = "Bearer bgc0b8awbwas6g5g5k5o5s5w606g37w3cc3bo3b83k39s3co3c83c03ck";
-            fetchTodosAndRender();
-
-        });
+        renderLoginComponent( { appEl, setToken: (newToken) => {token = newToken}, fetchTodosAndRender } )
 
         return;
         
-      }
+      };
+
       const tasksHtml = tasks
         .map((task) => {
           return `
